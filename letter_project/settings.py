1dab001d4cc1b1b9acade79cc6df1bfa71e9b91b
Yeongeunn <nye0817@gmail.com>
2025-05-16 21:53:13 +0900

    doc: gitignore, requirements 추가

--- letter_project/settings.py
+++ letter_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'letter_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '본인 비밀번호',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
