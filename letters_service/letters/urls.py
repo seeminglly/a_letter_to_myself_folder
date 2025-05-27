a2f6756fcbeefcfa1a053af3e4a707d1ffc3ec33
kmjm231 <kmjm231@gmail.com>
2025-05-28 00:14:41 +0900

    Letters msa 분리

--- letters_service/letters/urls.py
+++ letters_service/letters/urls.py
@@ -24,12 +24,12 @@ from django.conf import settings
 
 app_name = "letters" 
 urlpatterns = [
-    path('writing/', views.write_letter, name="writing"),
-    path('letters/', views.letter_list, name='letter_list'),  # 작성한 편지 목록
-    path('<int:letter_id>/', views.letter_json, name="letter_json"),
-    path('<int:letter_id>/delete/', views.delete_letter_api_internal, name='delete_letter_api_internal'), # 편지 삭제 API 엔드포인트 (내부 API)
-   
+    path('write/', views.write_letter, name="writing"), # letters/write
+    path('', views.letter_list, name='letter_list'),  # 작성한 편지 목록 letters/
+    path('api/letters/<int:letter_id>/', views.letter_json, name="letter_json"),
+    path('delete/<int:letter_id>/', views.delete_letter_api_internal, name='delete_letter_api_internal'), # 편지 삭제 API 엔드포인트 (내부 API)
 ] 
+
 # 개발 중일 때만 미디어 파일 서빙
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
