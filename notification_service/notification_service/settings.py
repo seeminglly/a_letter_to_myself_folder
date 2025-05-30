3669e5adf8c990a9260226bb7e7cc220955fbb42
Yeongeunn <nye0817@gmail.com>
2025-05-30 19:03:40 +0900

    feat: 알림 마이크로서비스 구현 (POST 요청 → Celery → Gmail 메일 전송)

--- notification_service/notification_service/settings.py
+++ notification_service/notification_service/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,3 +69,12 @@ CELERY_BROKER_URL = 'amqp://localhost' # Celery가 메시지 브로커로 Rabbit
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 DEFAULT_FROM_EMAIL = 'admin@example.com'  # 이메일 테스트용
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
